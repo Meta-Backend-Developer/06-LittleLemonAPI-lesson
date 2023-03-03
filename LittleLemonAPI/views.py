edfb5b013664df570226cbaea5ff04fc91ae2399
mitchcamza <192mitch88@gmail.com>
2023-03-03 07:42:50 +0200

    Add serializers for menu-items

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,12 +1,18 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import MenuItem
 from .serializers import MenuItemSerializer
-
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+from django.shortcuts import get_object_or_404
 
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
+
+@api_view()
+def single_item(request, pk):
+    item = get_object_or_404(MenuItem, id=pk)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
