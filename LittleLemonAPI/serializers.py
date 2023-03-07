a208fd1b3293a315ac4520637f9bdf79acf2d3d2
mitchcamza <192mitch88@gmail.com>
2023-03-07 07:09:17 +0200

    Add HyperlinkedModelSerializer for menu-items

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -7,7 +7,7 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id','slug','title']
 
-class MenuItemSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
 
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,14 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 from django.shortcuts import get_object_or_404
 
 
 @api_view()
 def menu_items(request):
     items = MenuItem.objects.select_related('category').all()
-    serialized_item = MenuItemSerializer(items, many=True)
+    serialized_item = MenuItemSerializer(items, many=True, context={'request': request})
     return Response(serialized_item.data)
 
 @api_view()
@@ -16,3 +16,9 @@ def single_item(request, pk):
     item = get_object_or_404(MenuItem, id=pk)
     serialized_item = MenuItemSerializer(item)
     return Response(serialized_item.data)
+
+@api_view()
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    serialized_category = CategorySerializer(category)
+    return Response(serialized_category.data)
