2d339094d3b65bf88555f673fd1cbd22f0ab7152
mitchcamza <192mitch88@gmail.com>
2023-03-08 15:02:08 +0200

    Validate input data and map it to an existing model using DRF validation and deserialization

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,15 +1,24 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import MenuItem, Category
 from .serializers import MenuItemSerializer, CategorySerializer
 from django.shortcuts import get_object_or_404
 
 
-@api_view()
+@api_view(['Get','POST'])
 def menu_items(request):
-    items = MenuItem.objects.select_related('category').all()
-    serialized_item = MenuItemSerializer(items, many=True, context={'request': request})
-    return Response(serialized_item.data)
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items, many=True, context={'request': request})
+        return Response(serialized_item.data)
+    
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data, context={'request': request})
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
+
 
 @api_view()
 def single_item(request, pk):
