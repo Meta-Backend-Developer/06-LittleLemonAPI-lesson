a40d9d4be436dc01296d4307972adb94eb2936f3
mitchcamza <192mitch88@gmail.com>
2023-03-14 07:27:44 +0200

    Add TemplateHTMLRenderer to display menu

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('menu-items', views.menu_items),
     path('menu-items/<int:pk>', views.single_item),
+    path('menu', views.menu),
     path('category/<int:pk>', views.category_detail, name='category-detail'),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import status
 from .models import MenuItem, Category
 from .serializers import MenuItemSerializer, CategorySerializer
@@ -19,7 +20,6 @@ def menu_items(request):
         serialized_item.save()
         return Response(serialized_item.data, status.HTTP_201_CREATED)
 
-
 @api_view()
 def single_item(request, pk):
     item = get_object_or_404(MenuItem, id=pk)
@@ -31,3 +31,10 @@ def category_detail(request, pk):
     category = get_object_or_404(Category, pk=pk)
     serialized_category = CategorySerializer(category)
     return Response(serialized_category.data)
+
+@api_view()
+@renderer_classes([TemplateHTMLRenderer])
+def menu(request):
+    items = MenuItem.objects.select_related('category').all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response({'data':serialized_item.data}, template_name='menu-items.html')
