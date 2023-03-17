871a4bfc7548d89d8fc84aeaec84ee00364b062e
mitchcamza <192mitch88@gmail.com>
2023-03-17 05:50:55 +0200

    Add validation for MenuItemSerializer: Add field conditions for stock and price, and UniqueValidator for title.

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import MenuItem, Category
 from decimal import Decimal
 
@@ -8,10 +9,15 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','slug','title']
 
 class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
-    stock = serializers.IntegerField(source='inventory')
+    stock = serializers.IntegerField(source='inventory', min_value=0)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     category = CategorySerializer(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
+    title = serializers.CharField(
+        max_length=255, 
+        validators = [UniqueValidator(queryset=MenuItem.objects.all())]
+        )
     class Meta:
         model = MenuItem
         fields = ['id','title','price','stock','price_after_tax','category','category_id']
